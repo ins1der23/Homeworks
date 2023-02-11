47fc7bb03641dde80fff6114ee7bb35c24f00bfd
ins1der23 <m.zadorin@uopt.ru>
2023-02-11 23:12:29 +0500

    Added homeworks lang_first_steps, more_less done

---
+++ lang_first_steps/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два числа, я подумаю и скажу,какое больше");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+if (a != b)
+{
+    if (a > b)
+    {
+        Console.WriteLine("Первое число больше второго ");
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
