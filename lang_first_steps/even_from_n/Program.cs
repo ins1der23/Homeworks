98b822be262d0be371570e067ecafe27d19dd889
ins1der23 <m.zadorin@uopt.ru>
2023-02-12 14:03:53 +0500

    Even_from_n done

---
+++ lang_first_steps/even_from_n/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());;
+Console.WriteLine("Сейчас вы увидите магию");
+if(n>0)
+{
+    if(n%2!=0)
+    {
+        n=n-1;
+    }
+    while(n>=2)
+    {
+        Console.WriteLine(n);
+        n=n-2;
+    }
+}
+else
+{
+    if(n%2!=0)
+    {
+        n=n+1;
+    }
+    while(n<=0)
+    {
+        Console.WriteLine(n);
+        n=n+2;
+    }
+}
