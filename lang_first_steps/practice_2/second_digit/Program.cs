99f073f5df4e7ab142f7f0cbf62ca336efc1ee04
ins1der23 <m.zadorin@uopt.ru>
2023-02-15 10:49:44 +0500

    HW gor practice_2 lang_first_steps started

---
+++ lang_first_steps/practice_2/second_digit/Program.cs
@@ -0,0 +1,19 @@
+﻿// **Задача 10:** Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **вторую** цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int SecondDigit (int Any3DgNumber)
+{
+    return Any3DgNumber / 10 % 10;
+}
+Console.WriteLine("Введите трехзначноечисло, увидите чудо");
+Console.Write("Число = ");
+int SomeNumber = int.Parse(Console.ReadLine());
+while(SomeNumber<100 || SomeNumber>1000)
+{
+    Console.WriteLine("Так вы чуда не увидите. Введите ТРЕХЗНАЧНОЕ число");
+    Console.Write("Число = ");
+    SomeNumber = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Чудо = {0}", SecondDigit(SomeNumber));
