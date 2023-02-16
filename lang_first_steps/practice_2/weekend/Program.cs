81395ee4e5fc2074d8f8ae0786268f02d06cfbe0
ins1der23 <m.zadorin@uopt.ru>
2023-02-16 08:53:32 +0500

    Weekend done

---
+++ lang_first_steps/practice_2/weekend/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// **Задача 15:** Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool WeekendCheck(int DayNumber)
+{
+    return DayNumber > 5;
+}
+
+Console.WriteLine("Введите число, обозначающие день недели");
+int CheckedNumber = int.Parse(Console.ReadLine());
+while (CheckedNumber < 1 || CheckedNumber > 7)
+{
+    Console.WriteLine("Не является днем недели");
+    Console.Write("Введите число еще раз ");
+    CheckedNumber = int.Parse(Console.ReadLine());
+}
+
+if (WeekendCheck(CheckedNumber)) Console.WriteLine("Выходной");
+else Console.WriteLine("Не выходной");
+
